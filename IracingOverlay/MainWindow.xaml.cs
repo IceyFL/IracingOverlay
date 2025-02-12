684cf7dae18d5d81862dcda2185dbdc098fc5338
owenlol2132 <owenthetrueman@gmail.com>
2025-02-12 22:13:02 +0000

    LapDistance
    Added a text block to the window.
    It updates the distance to the 6th car every time it recieves signal

--- IracingOverlay/MainWindow.xaml.cs
+++ IracingOverlay/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,14 +20,86 @@ using IRSDKSharper;
 
 namespace IracingOverlay
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        private IRacingSdk irsdk;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // create an instance of IRacingSdk
+            irsdk = new IRacingSdk();
+
+            // hook up our event handlers
+            irsdk.OnException += OnException;
+            irsdk.OnConnected += OnConnected;
+            irsdk.OnDisconnected += OnDisconnected;
+            irsdk.OnSessionInfo += OnSessionInfo;
+            irsdk.OnTelemetryData += OnTelemetryData;
+            irsdk.OnStopped += OnStopped;
+            irsdk.OnDebugLog += OnDebugLog;
+
+            // this means fire the OnTelemetryData event every 30 data frames (2 times a second)
+            irsdk.UpdateInterval = 30;
+
+            // lets go!
+            irsdk.Start();
         }
+
+        #region EventHandlers
+
+        //debugging stuff
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            irsdk.Stop();
+        }
+
+        private void OnException(Exception exception)
+        {
+            Debug.WriteLine("OnException() fired!");
+        }
+
+        private void OnConnected()
+        {
+            Debug.WriteLine("OnConnected() fired!");
+        }
+
+        private void OnDisconnected()
+        {
+            Debug.WriteLine("OnDisconnected() fired!");
+        }
+
+        private void OnSessionInfo()
+        {
+            var trackName = irsdk.Data.SessionInfo.WeekendInfo.TrackName;
+
+            Debug.WriteLine($"OnSessionInfo fired! Track name is {trackName}.");
+        }
+
+        private void OnTelemetryData()
+        {
+            var lapDistPct = irsdk.Data.GetFloat("CarIdxLapDistPct", 5);
+
+            Dispatcher.Invoke(() =>
+            {
+                Delta.Text = $"Lap dist pct for the 6th car in the array is {lapDistPct}.";
+            });
+        }
+
+        private void OnStopped()
+        {
+            Debug.WriteLine("OnStopped() fired!");
+        }
+
+        private void OnDebugLog(string message)
+        {
+            Debug.WriteLine(message);
+        }
+
+        #endregion
+
     }
 }

e061837d51b7bdb9b83b743a9d266f871a2edc12
owenlol2132 <owenthetrueman@gmail.com>
2025-02-23 22:58:00 +0000

    Setup UI element to display user info
    Setup base for UI element

--- IracingOverlay/Reference.xaml.cs
+++ IracingOverlay/Reference.xaml.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using IracingOverlay.UI;
 
 namespace IracingOverlay
 {
@@ -10,6 +11,8 @@ namespace IracingOverlay
         public Reference()
         {
             InitializeComponent();
+            var Driver1 = new ReferenceUser("Reference");
+            mainGrid.Children.Add(Driver1);
         }
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
---
+++ IracingOverlay/UI/ReferenceUser.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace IracingOverlay.UI
+{
+    /// <summary>
+    /// Interaction logic for ReferenceUser.xaml
+    /// </summary>
+    public partial class ReferenceUser : UserControl
+    {
+        public ReferenceUser(string Text)
+        {
+            InitializeComponent();
+            Name.Content = Text;
+        }
+    }
+}

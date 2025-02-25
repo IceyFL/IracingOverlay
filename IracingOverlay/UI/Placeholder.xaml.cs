207acc1749cac22d3c7db68d2b83b3c17051be9b
owenlol2132 <owenthetrueman@gmail.com>
2025-02-25 02:15:19 +0000

    Added Placeholder for UI
    Added placeholder UI element to fill empty space

---
+++ IracingOverlay/UI/Placeholder.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace IracingOverlay.UI
+{
+    /// <summary>
+    /// Interaction logic for Placeholder.xaml
+    /// </summary>
+    public partial class Placeholder : UserControl
+    {
+        public Placeholder()
+        {
+            InitializeComponent();
+        }
+    }
+}

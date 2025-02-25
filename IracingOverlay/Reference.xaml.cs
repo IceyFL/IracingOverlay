9ad4c474b3fb0678b47078f91dfa4e8810b09209
owenlol2132 <owenthetrueman@gmail.com>
2025-02-25 23:33:19 +0000

    Fixed another error
    Made all the UI reset and load at the same time so their is no frames inbetween that make the program look buggy

--- IracingOverlay/Reference.xaml.cs
+++ IracingOverlay/Reference.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Input;
@@ -8,6 +9,9 @@ namespace IracingOverlay
 {
     public partial class Reference : Window
     {
+        //list to store all the UI elements
+        private List<UIElement> elements = new List<UIElement>();
+
         public Reference()
         {
             InitializeComponent();
@@ -19,7 +23,7 @@ namespace IracingOverlay
             var Driver = new ReferenceUser(position, drivername, safetyrating, licenselevel, irating, delta);
 
             //add it to the UI
-            mainGrid.Children.Add(Driver);
+            elements.Add(Driver);
         }
 
         public void Placeholder() {
@@ -28,7 +32,7 @@ namespace IracingOverlay
             var empty = new Placeholder();
 
             //add to the UI
-            mainGrid.Children.Add(empty);
+            elements.Add(empty);
 
         }
 
@@ -36,6 +40,14 @@ namespace IracingOverlay
         {
             // Clear all children from the mainGrid
             mainGrid.Children.Clear();
+
+            //add all the elements from the list to the mainGrid
+            foreach (var element in elements)
+            {
+                mainGrid.Children.Add(element);
+            }
+
+            elements.Clear(); // reset the list
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
